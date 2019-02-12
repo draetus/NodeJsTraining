40bfe24f6f1e6123d47ce71f4c82000efaa99773
Mauricio Macario de Farias Junior <mauricio199836@hotmail.com>
2019-02-12 16:47:15 -0200

    Added date script

--- demo_module.js
+++ demo_module.js
@@ -3,5 +3,6 @@ var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
 }).listen(8080);
